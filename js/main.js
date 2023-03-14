365b2f9397c1159351a4d7662913668806249960
trent130 <warsamegift@gmail.com>
2023-03-14 15:54:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc =>{
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(t =>{
+            t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
+    })
+})
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.filters__content`, {delay: 900})
+sr.reveal(`.filters`, {delay: 1000})
